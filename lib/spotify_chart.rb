0a140e3d59b02f3360ca509924c7a0e94e770cac
brianneking86 <brianneking86@gmail.com>
2014-10-08 13:52:41 -0400

    through most_streamed

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,21 +1,24 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
 
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
     # return a string that is the base url + / + preference + / + region + / + latest
+    pref_url = "#{@base_url}#{preference}/#{region}/latest"
   end
 
   def get_json(url)
     # load json given a url here
     # refer to the references if you have questions about this
+    JSON.parse(`curl #{url}`)
   end
 
   def fetch_track_album_artist(music_hash)
@@ -42,17 +45,26 @@ class SpotifyChart
   
     # the track name, artist name, and album name should be the first track in the
     # tracks array
+      
+    track = music_hash["tracks"].first
+    str = "'#{track["track_name"]}' by #{track["artist_name"]} from the album #{track["album_name"]}"
+
   end
 
 
   def most_streamed(region)
-    # call no get_url here, where preference is the string 'most_streamed',
+    # call on get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
 
     # call on get_json here, using the string that get_url returns
     
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+    str = get_url("most_streamed", region)
+
+    hsh = get_json(str)
+
+    track = fetch_track_album_artist(hsh)
   end
 
   def most_shared(region)
